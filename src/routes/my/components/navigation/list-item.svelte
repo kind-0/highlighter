373b47e73b479c48bc927fdfabf1f86e545d3731
Soren <avantwebdev@protonmail.com>
2023-06-15 14:35:43 +0300

    save changes

--- src/routes/my/components/navigation/list-item.svelte
+++ src/routes/my/components/navigation/list-item.svelte
@@ -8,8 +8,7 @@
     export let item: NDKList;
     // export let lists: NDKList[];
 
-
-    console.log("list to render:", item)
+    console.log('list to render:', item);
     let hover = false;
 
     async function addToList(e: DragEvent) {
@@ -21,35 +20,31 @@
 
         // encrypted notes are added to encrypted tags
         if (kind === '4') {
-            await listEvent.addItem(tag, undefined, true);
+            await item.addItem(tag, undefined, true);
         } else {
-            if (listEvent.tags.find(t => t[1] === id)) {
+            if (item.tags.find((t) => t[1] === id)) {
                 alert('already has it');
                 return;
             }
 
-            await listEvent.addItem(tag);
+            await item.addItem(tag);
         }
 
-        listEvent.created_at = Math.floor(Date.now() / 1000);
-        await listEvent.sign();
-        listEvent.publish();
+        item.created_at = Math.floor(Date.now() / 1000);
+        await item.sign();
+        item.publish();
     }
 
     function tagIsList(tag: NDKTag) {
-        return tag[0] === 'a' && (
-            tag[1].startsWith('30000:') ||
-            tag[1].startsWith('30001:') ||
-            tag[1].startsWith('30022:')
-        );
+        return tag[0] === 'a' && (tag[1].startsWith('30000:') || tag[1].startsWith('30001:') || tag[1].startsWith('30022:'));
     }
 
     function decendants(list: NDKList) {
         const decendants = [];
 
-        for (const tag of listEvent.tags) {
+        for (const tag of item.tags) {
             if (tagIsList(tag)) {
-                const list = allLists.find(l => l.id === tag[1]);
+                const list = $sortedLists.find((l) => l.id === tag[1]);
                 if (list) {
                     decendants.push(list);
                 }
@@ -76,17 +71,23 @@
     <div
         draggable={true}
         on:dragstart={dragStart}
-        on:dragenter={() => hover = true}
-        on:dragleave={() => hover = false}
-        on:drop={e => {addToList(e); hover = false;}}
+        on:dragenter={() => (hover = true)}
+        on:dragleave={() => (hover = false)}
+        on:drop={(e) => {
+            addToList(e);
+            hover = false;
+        }}
         ondragover="return false"
         class="
             {hover ? 'bg-gray-100' : ''}
         "
     >
         <NavigationButton route="/my/lists/{item.encode()}">
-            <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600">{item.name.slice(0,1)}</span>
-                <span class="truncate">{item.name}</span>
+            <span
+                class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600"
+                >{item.name.slice(0, 1)}</span
+            >
+            <span class="truncate">{item.name}</span>
         </NavigationButton>
     </div>
 
@@ -97,5 +98,4 @@
             {/each}
         </ul>
     {/if} -->
-
 </li>
