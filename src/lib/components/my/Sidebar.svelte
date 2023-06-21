a02030fa726189db3f199a7e28e88461681ddaf9
Soren <avantwebdev@protonmail.com>
2023-06-21 13:29:25 +0300

    fix: side-list scroll

--- src/lib/components/my/Sidebar.svelte
+++ src/lib/components/my/Sidebar.svelte
@@ -102,12 +102,12 @@
                 </div>
             {/if}
 
-            <li>
+            <li class="list-container">
                 <div class="text-xs font-semibold leading-6 text-gray-400">Your lists</div>
                 {#if renderedList}
 
                     <ul class="-mx-2 mt-2  ">
-                        <div class="scrollable-lists pb-8">
+                        <div class="scrollable-lists pb-8 ">
                         {#each renderedList ?? [] as item}
                             {#if isTopLevel(item)}
                                 <ListItem {item} />
@@ -171,4 +171,11 @@
         overflow: auto;
     }
 
+.list-container {
+    flex-shrink: 1;
+    flex-grow: 1;
+    flex-basis: 200px;
+    overflow-y: auto;
+    overflow-x: hidden;
+}
 </style>
