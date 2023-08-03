d3b3744b476c2cccd37392cb78320ffbab5a6a53
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-08-02 20:36:58 -0600

    updated responsive logo in navbar

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -7,6 +7,8 @@
     import { currentUser } from '$lib/store';
 
     export let isHiddenSidebar = false;
+    export let isHiddenDrawerBtn = false;
+    export let isHiddenRelayBtn = false;
 
     let homeLink = '/';
     let navbarOpened = false;
@@ -20,17 +22,18 @@
         flex-row items-center sm:gap-4 navbar-start
     ">
         <div class="flex-none {!isHiddenSidebar ? 'lg:hidden' : ''}">
-            <label for="my-drawer" class="btn btn-square btn-ghost">
+            <label for="my-drawer" class="btn btn-square btn-ghost {isHiddenDrawerBtn ? 'hidden' : ''}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"
                     ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg
                 >
             </label>
         </div>
 
-
-        <a href={homeLink}>
+        <a href={homeLink} class="w-full">
             <div class="flex flex-shrink-0 items-center">
                 <div class="w-48 hidden sm:block"><FullLogo /></div>
+            </div>
+            <div class="w-full flex justify-center">
                 <div class="sm:hidden"><Logo /></div>
             </div>
         </a>
@@ -48,7 +51,7 @@
         {navbarOpened ? 'hidden md:flex' : 'flex'}
     ">
         <div class="flex items-center md:order-2 gap-4">
-            <div class="hidden md:block">
+            <div class="hidden {isHiddenRelayBtn ? '' : 'md:block'}">
                 <RelaysButton iconOnly={true} />
             </div>
             <CurrentUser />
