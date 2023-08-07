3cd5c95d5da12926152950fe3ca3f87cec61ba72
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-08-07 00:10:05 -0600

    Added notification component

--- src/lib/components/UserDropdown.svelte
+++ src/lib/components/UserDropdown.svelte
@@ -5,6 +5,7 @@
     import ndk from '$lib/stores/ndk';
     import GearIcon from '$lib/icons/Gear.svelte';
     import { logout } from '$lib/currentUser';
+    import Notification from './Notification.svelte';
 </script>   
    
    
@@ -19,7 +20,7 @@
             />
         </div>
 
-        <ul slot="dropdown-content" class="divide-y divide-neutral-800 menu p-0 rounded-box">
+        <ul slot="dropdown-content" class="divide-y divide-neutral-800 menu p-0 w-[245px] rounded-box">
             <div>
                 <div class="flex justify-between items-center menu-title px-[22px] py-[19px] gap-2">
                     <div class="w-0 flex flex-grow flex-row items-center gap-4">
@@ -27,7 +28,7 @@
                             ndk={$ndk}
                             user={$currentUser}
                             class="
-                                w-6 h-6 border-2 border-base-300 rounded-full
+                                w-8 h-8 border-2 border-base-300 rounded-full
                             "
                         />
                         <div class="text-center text-base text-base-100-content font-medium truncate">{$currentUser?.npub}</div>
@@ -35,7 +36,17 @@
                     <GearIcon />
                 </div>
             </div>
-            <div class="p-4">
+            <div class="py-4 px-5">
+                <span class="text-[10px] font-semibold tracking-widest">
+                    NOTIFICATIONS
+                </span>
+                <div class="flex flex-row py-3 justify-between">
+                    <!-- TODO UPDATE FUNCTIONAL NOTIFICATIONS COMPONENT -->
+                    <Notification title={"Zaps"} notifications={6} />
+                    <Notification title={"Likes"} />
+                    <Notification title={"Comments"} />
+                    <Notification title={"Unknown"} />
+                </div>
             </div>
             <div class="p-[22px]">
                 <div on:click={logout} class="btn btn-rounded-full rounded-full w-full h-11 bg-base-300">
