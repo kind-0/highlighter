815c9a223c45dde768ad1e9819fbbefcdedb3fde
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-08-11 18:18:00 -0600

    update circualIconButton styles

--- src/lib/components/buttons/CircularIconButton.svelte
+++ src/lib/components/buttons/CircularIconButton.svelte
@@ -10,15 +10,9 @@
 </script>
 
 <div class="flex flex-col items-center">
-    {#if group === value}
-        <button on:click={onClick} class="w-11 h-11 btn-circle bg-base-300 text-accent mb-2 grid place-items-center border border-accent hover:bg-accent hover:bg-opacity-20">
-            <slot />
-        </button>
-    {:else}
-        <button on:click={onClick} class="w-11 h-11 btn-circle bg-base-300 text-zinc-400 mb-2 grid place-items-center hover:border hover:border-zinc-400 hover:bg-opacity-30">
-            <slot />
-        </button>
-    {/if}
+    <button on:click={onClick} class="w-11 h-11 btn-circle bg-base-300 mb-2 grid place-items-center {group===value ? 'text-accent border border-accent hover:bg-accent hover:bg-opacity-20' : 'text-zinc-400 hover:border hover:border-zinc-400 hover:bg-opacity-30'} ">
+        <slot />
+    </button>
     <span class="text-xs font-normal text-base-100-content">
         {title}
     </span>
