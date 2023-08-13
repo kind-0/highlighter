e44d8cc339a6254e73a4dc42482fb5837ccb776d
pablof7z <p@f7z.io>
2023-08-13 13:46:15 +0300

    Improve general button

--- src/lib/components/buttons/GeneralButton.svelte
+++ src/lib/components/buttons/GeneralButton.svelte
@@ -1,7 +1,6 @@
-<script lang="ts">
-    export let handleClick = () => {};
-</script>
-
-<button on:click={handleClick} class="btn btn-outline btn-rounded-full rounded-full border-accent2 bg-base-200 text-base-100-content text-sm md:text-base hover:border-accent2 hover:bg-accent2 hover:bg-opacity-20 hover:text-base-100-content">
-    <slot name="btn-content">General Button</slot>
+<button on:click class="
+    btn btn-outline btn-rounded-full rounded-full border-accent2 bg-base-200 text-base-100-content text-sm md:text-base hover:border-accent2 hover:bg-accent2 hover:bg-opacity-20 hover:text-base-100-content
+    {$$props.class??""}
+">
+    <slot />
 </button>
